fedb3271fa14a1c67cee662bf4bcfc4a1e9eb3a4
Shah Ehsan <shahehsan2002@gmail.com>
2024-11-04 22:39:03 +0600

    error back pattern creaing for middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,20 +1,29 @@
-import { NextRequest } from "next/server";
+import { cookies } from "next/headers";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function middleware(request: NextRequest) {
-    console.log(request,"Middleware");
+  console.log(request, "Middleware");
 }
+
+const { pathname } = request.nextUrl;
+
+const accessToken = cookies().get("accessToken")?.value;
+
+if (!accessToken) {
+  return NextResponse.redirect(new URL("/login"));
+}
+
+// error back pattern
 
 export const config = {
-    matcher: [
-        "/login",
-        "/register",
-        "/dashboard/:page*",
-        "/admin-dashboard/:page*",
-        "/driver-dashboard/:page*",
-        
-    ]
-}
-
+  matcher: [
+    "/login",
+    "/register",
+    "/dashboard/:page*",
+    "/admin-dashboard/:page*",
+    "/driver-dashboard/:page*",
+  ],
+};
 
 //PUBLIC ROUTES
 //PUBLIC PROTECTED ROUTES
